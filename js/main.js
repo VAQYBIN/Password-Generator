728d77e9bad2e4d110c5950185a64e75dd6eb4d1
VAQYBIN <63801831+VAQYBIN@users.noreply.github.com>
2020-06-24 21:53:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getPassword() {
+  var chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+  var password = "";
+  var passwordLength = 16;
+
+  for (var i=0; i<passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber,randomNumber+1);
+  }
+    document.getElementById("password").value = password
+}
